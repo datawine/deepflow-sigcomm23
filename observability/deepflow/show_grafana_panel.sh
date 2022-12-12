38fc2137eadbe71bc70ca8be91aa4036d459ce20
futrime <futrime@futrime.com>
2022-12-12 01:29:59 +0000

    Add SSH tunnel hints

--- observability/deepflow/show_grafana_panel.sh
+++ observability/deepflow/show_grafana_panel.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-pushd .
-
-cd ~
+pushd ~ > /dev/null
 
 ####################################################
 
 NODE_PORT=$(kubectl get --namespace deepflow -o jsonpath="{.spec.ports[0].nodePort}" services deepflow-grafana)
 NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
 echo -e "Grafana URL: http://$NODE_IP:$NODE_PORT  \nGrafana auth: admin:deepflow"
+echo -e "\nIf you would like to access via SSH tunnel, please run the following command on your local computer:"
+echo -e "\n   ssh -L $NODE_PORT:localhost:$NODE_PORT $USER@$NODE_IP"
+echo -e "\nThen open your browser and go to <http://localhost:$NODE_PORT>"
 
 ####################################################
-
-echo "Grafana panel is shown above."
 
 popd > /dev/null
