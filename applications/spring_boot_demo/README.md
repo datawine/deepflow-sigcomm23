e7521bca0cddf80ab6f269b1aae06e4380a6b760
futrime <futrime@futrime.com>
2022-12-15 02:43:54 +0000

    Add automatic getting IP

--- applications/spring_boot_demo/README.md
+++ applications/spring_boot_demo/README.md
@@ -4,6 +4,6 @@
 
 运行`source undeploy.sh`以拆卸。
 
-运行`source test.sh <url> <rate>`以测试。其中`<url>`是`foo-svc`服务所部署的IP地址，`<rate>`是吞吐率。
+运行`source test.sh <rate>`以测试。其中`<rate>`是吞吐率。
 
 参考<https://deepflow.yunshan.net/docs/zh/auto-tracing/spring-boot-demo/>。
--- applications/spring_boot_demo/test.sh
+++ applications/spring_boot_demo/test.sh
@@ -5,14 +5,16 @@ pushd ~ > /dev/null
 ####################################################
 
 # Return if some parameters are missing
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: source test.sh <url> <rate>"
+if [ -z "$1" ]; then
+    echo "Usage: source test.sh <rate>"
     popd > /dev/null
     return 0
 fi
 
+IP_ADDR=$(kubectl get service/foo-svc -n deepflow-ebpf-spring-demo -o jsonpath='{.spec.clusterIP}')
+
 kubectl create job -n deepflow-ebpf-spring-demo my-load-generator --image=futrime/wrk2 -- \
-    wrk --connections 16 --duration 30s --latency --rate $2 $1
+    wrk --connections 16 --duration 30s --latency --rate $1 http://$IP_ADDR
 
 echo "Waiting for job to complete..."
 
